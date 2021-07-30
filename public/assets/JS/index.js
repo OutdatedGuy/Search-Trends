9df068be1f63beffe069270f51f80a81e122d03b
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-07-31 02:45:48 +0530

    Added auto input field adding

--- public/assets/JS/index.js
+++ public/assets/JS/index.js
@@ -1,2 +1,45 @@
 import { getTrends } from "./api/getTrends.js";
-document.getElementById("button").addEventListener("click", getTrends);
+
+document.addEventListener("DOMContentLoaded", addInput.bind(this, 0));
+document.getElementById("input-container").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const inputs = document.getElementsByClassName("word input");
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].value.length === 0 || !inputs[i].value.trim()) {
+        document.getElementById(inputs[i].id).remove();
+        console.log(inputs.length);
+        i--;
+      }
+    }
+
+    getLoading(true);
+    getTrends().then(() => {
+      if (inputs.length < 5) {
+        addInput(inputs.length);
+      }
+      getLoading(false);
+    });
+  }
+});
+
+function addInput(id) {
+  const input = document.createElement("input");
+  input.classList.add("word");
+  input.classList.add("input");
+  input.id = `input-box${id + 1}`;
+  input.type = "text";
+  input.placeholder = "Enter a Word here!";
+  document.getElementById("input-container").appendChild(input);
+}
+
+function getLoading(start) {
+  if (start) {
+    document.getElementById("barChart").style.display = "none";
+    document.getElementById("lineChart").style.display = "none";
+    document.getElementById("loading-container").style.display = "flex";
+  } else {
+    document.getElementById("loading-container").style.display = "none";
+    document.getElementById("barChart").style.display = "inline-block";
+    document.getElementById("lineChart").style.display = "inline-block";
+  }
+}
