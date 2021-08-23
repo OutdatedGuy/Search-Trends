b0c3545e147a1703c7fb4b1227de45e59bbbc072
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2021-08-24 00:50:03 +0530

    Allowed cross origin requests

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from "express";
 import { trends } from "./routes/trends.js";
 
 const app = express();
+
 const port = process.env.PORT || 1412;
 app.listen(port, () => {
   console.log(`Server listening on port http://127.0.0.1:${port}`);
 });
+
 app.use(express.static("public"));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.post("/trends", trends);
